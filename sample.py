e0ac5f293632dfb7fe4396807e7fbaf4762e9cb0
lahariduvvada@gmail.com <Lahari1826>
2025-11-24 14:33:56 +0530

    added python file

---
+++ sample.py
@@ -0,0 +1,11 @@
+# A simple Python program
+
+# Print a greeting
+print("Hello, world!")
+
+# Do a small calculation
+a = 5
+b = 3
+sum_result = a + b
+
+print("The sum of", a, "and", b, "is:", sum_result)
